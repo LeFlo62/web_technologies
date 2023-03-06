f1661303a7b3946bc431ba87d75e6e5094af1d84
LeFlo <leflodu62@live.fr>
2023-03-06 15:40:50 +0100

    First commit

---
+++ back/src/main/java/fr/isep/super_morpion/SuperMorpionApplication.java
@@ -0,0 +1,13 @@
+package fr.isep.super_morpion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMorpionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMorpionApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/fr/isep/super_morpion/SuperMorpionApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isep.super_morpion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMorpionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
