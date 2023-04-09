9d972a79a1046aa4bdd087fbf469d2c52cc6f123
LeFlo <github@email.leclercq-florentin.fr>
2023-04-09 19:48:00 +0200

    Fixed Home Page missing menuBar

--- front/adopte_1_logement/src/app/app.component.ts
+++ front/adopte_1_logement/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adopte_1_logement';
+
+  menuBar : MenuItem[] = [
+    {label: 'Accueil', icon: 'pi pi-fw pi-home', routerLink: ['/home']},
+    {label: 'Mes annonces', icon: 'pi pi-fw pi-tag', routerLink: ['/housing']},
+    {label: 'Mes locations', icon: 'pi pi-fw pi-calendar', routerLink: ['/user']},
+    {label: 'Messagerie', icon: 'pi pi-fw pi-comment', routerLink: ['/admin']},
+  ];
 }
