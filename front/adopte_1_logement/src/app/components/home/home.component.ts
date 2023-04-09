6f30b28adff4a731db9419ae539fe77bf2cb4bae
LeFlo <github@email.leclercq-florentin.fr>
2023-04-09 19:27:24 +0200

    Working on Home Page

---
+++ front/adopte_1_logement/src/app/components/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HousingListItem } from 'src/app/data/housing';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  housingList : HousingListItem[] = [];
+
+  constructor() {
+    for(let i : number = 0; i < 10; i++) {
+      this.housingList.push(
+        {
+          id: 'zdaz45d4s321dz81',
+          title: 'House ' + i,
+          author: 'Author ' + i,
+          rating: Math.random() * 5,
+          image: 'https://picsum.photos/300/300'
+        }
+      );
+    }
+  }
+
+}
--- front/super_morpion/src/app/app.module.ts
+++ front/super_morpion/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { MenubarModule } from 'primeng/menubar';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    FormsModule,
+    ButtonModule,
+    RatingModule,
+    AvatarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
