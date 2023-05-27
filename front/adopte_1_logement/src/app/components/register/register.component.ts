e6dbbc899d68b2aa582f5675ce2376685627bbba
Nathan WEBER <nathan.weber@eleve.isep.fr>
2023-05-27 22:38:04 +0200

    Added register form

---
+++ front/adopte_1_logement/src/app/components/register/register.component.ts
@@ -0,0 +1,86 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ValidatorService} from "../../services/validator.service";
+import {Router} from "@angular/router";
+import {MessageService} from "primeng/api";
+import {UserService} from "../../services/user.service";
+import {User} from "../../data/user";
+
+@Component({
+  selector: 'app-register', templateUrl: './register.component.html', styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  isSuccessful = false;
+  isSignUpFailed = false;
+  submitting: boolean = false;
+
+
+  registerForm: FormGroup = this.fb.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    password2: ['', [Validators.required]],
+  }, {
+    validators: [this.validatorService.equalControls('password', 'password2'),],
+  });
+
+  get loginPath() {
+    return "http://localhost:4200/login";
+  }
+
+
+  constructor(private fb: FormBuilder, private router: Router, private validatorService: ValidatorService, private authService: AuthService, private usersService: UserService) {
+  }
+
+
+  isInvalid(controlName: string) {
+    const control = this.registerForm.get(controlName);
+    return control?.invalid && (control?.dirty || control?.touched);
+  }
+
+
+  get firstName() {
+    return this.registerForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.registerForm.get('lastName');
+  }
+
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get password2() {
+    return this.registerForm.get('password2');
+  }
+
+  ngOnInit(): void {
+  }
+
+  register(): void {
+    this.submitting = true;
+    const { firstName, lastName, email, password } = this.registerForm.value;
+    this.authService.register(firstName, lastName, email, password).subscribe({
+      next: data => {
+        console.log(data);
+        this.submitting = false;
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      }, error: err => {
+        console.log(err);
+        this.submitting = false;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
+}
